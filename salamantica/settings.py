9f3fb9720546b1943bbedeb7b2bfcc2253eca35a
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2019-04-08 05:56:31 +0200

    mail configs done

--- salamantica/settings.py
+++ salamantica/settings.py
@@ -33,7 +33,7 @@ FIXTURE_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'albertosanmartinmartinez@gmail.com'
-EMAIL_HOST_PASSWORD = 'motoscoot.es7620'
+EMAIL_HOST_PASSWORD = config('MAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
